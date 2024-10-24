fa62964ee0cf9278574b1769cd5b75ef716d96e0
Shallowcat <joshR364@hotmail.com>
2024-10-24 19:30:02 -0400

    endings finished

--- Assets/Scripts/Car Ending.cs
+++ Assets/Scripts/Car Ending.cs
@@ -1,38 +1,48 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CarEnding : MonoBehaviour
 {
     public GameObject car;
     public GameObject cam;
-    public GameObject ending;
-    public GameObject restartButton;
-    void Start()
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject fade;
+
+    string dialog = "You left your family behind and never came back. THE END.";
+    int charNum;
+    string sentence;
+    int delay;
+
+
+    public void OnClick()
     {
-        ending.SetActive(false);
-        restartButton.SetActive(false);
-        StartCoroutine(Pause());
+        SceneManager.LoadScene(0);
     }
-    
-    void Update()
-    {
-        car.transform.Translate(Vector3.forward * 1.0f);
-    }
+   
 
     private void FixedUpdate()
     {
-        cam.transform.Rotate(-1f, 0f, 0f);
+        delay++;
+        car.transform.Translate(Vector3.forward * 1f);
+        cam.transform.Rotate(-0.5f, 0f, 0f);
+
+        if (dialog.Length > charNum && delay % 2 == 0)
+        {
+            sentence += dialog[charNum];
+            charNum++;
+            text.text = sentence;
+        }
+
+
+        var temp = fade.GetComponent<Image>().color;
+        temp.a += 0.01f;
+        fade.GetComponent<Image>().color = temp;
     }
 
-    IEnumerator Pause()
-    {
-        yield return new WaitForSeconds(2f);
-        ending.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        yield return new WaitForSeconds(2f);
-        restartButton.SetActive(true);
-    }
 }
--- Assets/Scripts/Milk Ending.cs
+++ Assets/Scripts/Milk Ending.cs
@@ -1,52 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MilkEnding : MonoBehaviour
 {
-    public GameObject cam;
-    public GameObject ending;
-    public GameObject restartButton;
-    public float camSpeed;
-    public GameObject[] dialog;
-    void Start()
-    {
-        ending.SetActive(false);
-        restartButton.SetActive(false);
-        StartCoroutine(Pause());
-        StartCoroutine(ShowDialog());
-        for (int i = 0; i < dialog.Length; i++)
-        {
-            dialog[i].SetActive(false);
-        }
-    }
     
+    public float camSpeed;
+
+    public GameObject cam;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] GameObject fade;
+
+    string dialog = "You returned home to your wife milk in hand hoping this marks a new begining. THE END.";
+    int charNum;
+    string sentence;
+    int delay;
+
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     void FixedUpdate()
     {
         cam.transform.Translate(Vector3.forward * camSpeed);
+
+        delay++;
+
+        if (dialog.Length > charNum && delay % 2 == 0)
+        {
+            sentence += dialog[charNum];
+            charNum++;
+            text.text = sentence;
+        }
+
+
+        var temp = fade.GetComponent<Image>().color;
+        temp.a += 0.01f;
+        fade.GetComponent<Image>().color = temp;
     }
     
-    IEnumerator Pause()
-    {
-        yield return new WaitForSeconds(10f);
-        for (int i = 0; i < dialog.Length; i++)
-        {
-            dialog[i].SetActive(false);
-        }
-        ending.SetActive(true);
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-        yield return new WaitForSeconds(2f);
-        restartButton.SetActive(true);
-    }
 
-    IEnumerator ShowDialog()
-    {
-        for (int i = 0; i < dialog.Length; i++)
-        {
-            Debug.Log(i.ToString());
-            dialog[i].SetActive(true);
-            yield return new WaitForSeconds(1.0f);
-        }
-    }
 }
